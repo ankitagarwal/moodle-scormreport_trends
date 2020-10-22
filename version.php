5705aef051a64a1aa41eead8939b9159b9411d00
Ankit Agarwal <ankit.agarwal@ab-inbev.com>
2020-10-22 18:02:28 +0530

    Release - 3.0

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019092300;
-$plugin->requires  = 2018120300; // Supports 3.5 and above.
+$plugin->version   = 2020102202;
+$plugin->requires  = 2019111800; // Supports 3.8 and above.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "2.0";
+$plugin->release   = '3.0';
 $plugin->component = 'scormreport_trends'; // Full name of the plugin (used for diagnostics).
 
